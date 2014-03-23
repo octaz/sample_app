e39e47f8a8f74cd8a9192319eae354c8232aef91
Thomas DeFina <tdef90@gmail.com>
2014-03-23 18:59:43 -0400

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ ruby '1.9.3'
 gem 'rails', '4.0.3'
 #gem 'bootstrap-sass', '2.3.2.0'
 gem 'bootstrap-sass'
+gem 'bcrypt-ruby'
+
 
 group :development, :test do
   gem 'sqlite3', '1.3.8'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,17 +2,38 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+  end
+  
+
 	let(:base_title) {"Ruby on Rails Tutorial Sample App"}
 
   subject {page}
+
+  shared_examples_for "all static pages" do
+    it {should have_selector('h1', text: heading)}
+    it {should have_title(full_title(page_title))}
+  end
 
   describe "Home page" do
 
     before {visit root_path}
 
-    it {should have_content('Sample App')}
-    it {should have_title(full_title(''))}
-    it {should_not have_title('| Home')}
+    let (:heading) { 'Sample App'}
+    let (:page_title) {''}
+
+    it_should_behave_like "all static pages"
+    it {should_not have_title ('| Home')}
+
+   # it {should have_content('Sample App')}
+   # it {should have_title(full_title(''))}
+   # it {should_not have_title('| Home')}
 
    # it "should have the content 'Sample App'" do
     #  visit root_path
@@ -33,8 +54,13 @@ describe "Static pages" do
   describe "Help Page" do
     before {visit help_path}
 
-    it {should have_content('Help')}
-    it {should have_title(full_title('Help'))}
+    let (:heading) {'Help'}
+    let (:page_title) {'Help'}
+
+    it_should_behave_like "all static pages"
+
+ #   it {should have_content('Help')}
+ #   it {should have_title(full_title('Help'))}
 
   	# it "shold have the content 'Help'" do
   	# 	visit help_path
@@ -69,7 +95,8 @@ describe "Static pages" do
 
     before {visit contact_path}
 
-    it {should have_content('Contact')}
+    it {should have_selector('h1', text: 'Contact')}
+  #  it {should have_content('Contact')}
     it {should have_title(full_title('Contact'))}
 
   	# it "should have the content 'Contact'" do
